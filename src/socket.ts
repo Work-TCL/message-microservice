2cf8efb905b83b7366d2dec3e16b3be8c3799f81
Akshay <akshay10sangani@gmail.com>
2025-05-29 22:11:14 +0530

    socket config changed

--- src/socket.ts
+++ src/socket.ts
@@ -9,9 +9,9 @@ const httpServer = createServer(app);
 // Initialize Socket.IO server with CORS configuration
 const io = new Server(httpServer, {
   cors: {
-    origin: ["https://trf.trinitysystems.in", "http://localhost:3000"], // Add real production origin
-    // origin: "*", // Change this to match the frontend's URL in production
-    methods: ["GET", "POST"],
+    // origin: ["https://trf.trinitysystems.in", "http://localhost:3000"], // Add real production origin
+    origin: "*", // Change this to match the frontend's URL in production
+    // methods: ["GET", "POST"],
   },
 });
 
